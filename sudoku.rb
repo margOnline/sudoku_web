3514c80230808e5f5cfa359b504a20506772495a
Margo <margo@margonline.co.uk>
2013-09-20 11:31:26 +0100

    setting up session variable

--- sudoku.rb
+++ sudoku.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
 
+enable :sessions
 
 def random_sudoku
   seed = (1..9).to_a.shuffle + Array.new(81-9,0)
@@ -10,7 +11,22 @@ def random_sudoku
   sudoku.to_s.chars
 end
 
+def puzzle(sudoku)
+  sudoku
+end
+
 get '/' do
-  @current_solution = random_sudoku
+  sudoku = random_sudoku
+  session[:solution] = sudoku
+  @current_solution = puzzle(sudoku)
   erb :index
+end
+
+get '/solution' do
+  @current_solution = session[:solution]
+  erb :index
+end
+
+get '/last-visit' do
+  "Previous visit to homepage: #{session[:last_visit]}"
 end
