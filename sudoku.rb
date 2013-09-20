2e22fa5d4242046c77e8b486e53cd6d2b9681b50
Margo <margo@margonline.co.uk>
2013-09-20 11:58:09 +0100

    colour_class helpers

--- sudoku.rb
+++ sudoku.rb
@@ -4,6 +4,25 @@ require_relative './lib/cell'
 
 enable :sessions
 
+helpers do
+  def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
+    must_be_guessed = puzzle_value == 0
+    tried_to_guess = current_solution_value.to_i != 0
+    guessed_incorrectly = current_solution_value != solution_value
+
+    if solution_to_check && must_be_guessed && tried_to_guess && guessed_incorrectly
+      'incorrect'
+    elsif !must_be_guessed
+      'value_provided'
+    end
+  end  
+
+  def cell_value
+    value.to_i == 0 ? '' : value
+  end
+
+end
+
 def random_sudoku
   seed = (1..9).to_a.shuffle + Array.new(81-9,0)
   sudoku = Sudoku.new(seed.join)
@@ -15,11 +34,33 @@ def puzzle(sudoku)
   sudoku
 end
 
-get '/' do
+def generate_new_puzzle_if_necessary
+  return if session[:current_solution]
   sudoku = random_sudoku
   session[:solution] = sudoku
-  @current_solution = puzzle(sudoku)
+  session[:puzzle] = puzzle(sudoku)
+  session[:current_solution] = session[:puzzle]
+end
+
+def prepare_to_check_solution 
+  @check_solution = session[:check_solution]
+  session[:check_solution] = nil
+end
+
+get '/' do
+  prepare_to_check_solution
+  generate_new_puzzle_if_necessary
+  @current_solution = session[:current_solution] || session[:puzzle]
+  @solution = session[:solution]
+  @puzzle = session[:puzzle]
   erb :index
+end
+
+post '/' do
+  cells = param["cells"]
+  session[:current_solution] = cells.map{|value| value.to_i}.join
+  session[:check_solution] = true
+  redirect to("/")
 end
 
 get '/solution' do
